885bdc4c81e52c6cb0282f4f5490efeac274f481
rbenites <rbenites115@gmail.com>
2018-08-03 09:22:01 -0700

    Login for UCLA project
    Testing out login authorization

---
+++ routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app){
+
+		const application = require('./routes/application');
+		const users = require('./routes/users');
+
+		app.use('/', application);
+		app.use('/users', users);
+    //other routes..
+}
